1b506a7a6fbf7e23a67217770b460833d7a2d2bc
zhangchiyu <2898180194@qq.com>
2020-04-09 12:15:53 +0800

    作业3 44-张驰誉 finish time stamp

--- post_youdao.py
+++ post_youdao.py
@@ -1,15 +1,29 @@
 import requests
 
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+
+def get_salt():
+    return '15855395889509'
+
+
+def get_sign():
+    return 'd8416f78b54601efd1bed7a202bee31c'
+
+
+def get_ts():
+    return '1585539588950'
+
+
 from_data={
     'i':'我和你',
     'from':'AUTO',
     'to':'AUTO',
     'smartresult':'dict',
     'client':'fanyideskweb',
-    'salt':'15855395889509',
-    'sign':'d8416f78b54601efd1bed7a202bee31c',
-    'ts':'1585539588950',
+    'salt': get_salt(),
+    'sign': get_sign(),
+    'ts': get_ts(),
     'bv':'70244e0061db49a9ee62d341c5fed82a',
     'doctype':'json',
     'version':'2.1',
---
+++ test_post_youdao.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import mock
+from post_youdao import *
+
+
+class PostYoudaoTest(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_get_ts(self):
+        import time
+        ts = time.time()
+        ts = str(int(round(ts * 1000)))
+        print(ts)
+        get_ts=mock.Mock(return_value='1585539588950')
+        self.assertEqual('1585539588950',get_ts())
+
+
+if __name__ == '__main__':
+    unittest.main()
